92981eaeb09fb633074a372fa7a759bc49923677
Shihab Al Ratul <mr.ratul66@gmail.com>
2021-02-08 14:48:07 +0100

    code improved

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ for (let i = 0; i <= 7; i++) {
 			const array = data.meals[0];
 			const menu = document.getElementById("menu");
 			const menuDiv = document.createElement("div");
-			menuDiv.className = '';
+			menuDiv.classList="p-4 bg-light meal-radius meal"
+			// menuDiv.addEventListener("click",function(){
+			// 	mealDetails('${array.idMeal}')
+			// })
 			const mealInfo = `
-			<div class="p-4 bg-light m-2 meal-radius">
 			<img class="w-100 meal-radius" src=${array.strMealThumb}>
 			<p class="d-flex justify-content-center pt-2 fw-bold">${array.strMeal}</p>
-			</div>
+			
 		`
 			menuDiv.innerHTML = mealInfo;
 			menu.appendChild(menuDiv);
@@ -22,6 +24,35 @@ for (let i = 0; i <= 7; i++) {
 
 		})
 }
+
+// const mealDetails = id => {
+// 	console.log(menu);
+// 	const contentBody = document.getElementById('content-body');
+// 	console.log(array);
+// 	contentBody.removeChild(menu);
+// 		fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+// 		.then(res => res.json())
+// 		.then(data => {
+// 			for(let i=1; i<=20;i++){
+// 			const array = data.meals[0];
+// 			const mealDetail = `
+// 			<img class="w-75 meal-radius" src=${array.strMealThumb}>
+// 			`
+// 		}
+// 		})
+// }
+
+
+// function mealDetails(){
+// 	let meal = document.getElementsByClassName("meal");
+// 	document.getElementById("menu").addEventListener("click", e=>{
+// 		if(e.target.matches(meal)){
+// 			document
+// 		}
+// 	})
+	
+	
+// }
 // function randomMeal(array) {
 // 	// for(let i=0; i<=8; i++){
 // 	console.log(array)
